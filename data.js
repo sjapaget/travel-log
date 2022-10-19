5af0482cdc822d5ef1d7b9560c1c55c05dd79420
Samuel Paget <sjapaget@gmail.com>
2022-10-19 13:18:40 +0200

    Add data file

---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    title: "Bristol",
+    location: "United Kingdom",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "30 Nov, 2022",
+    endDate: "4 Dec, 2022",
+    description: "Bristol is a city straddling the River Avon in the southwest of England with a prosperous maritime history.",
+    imageUrl: "https://lh4.googleusercontent.com/proxy/4_e3prjeYp-hf4TpUPgkRJxg21ydHXmO5X0yUExsGZRPM5SMpYFsVUT2RJxoHy57zTgOUVIngy0YCyPksTDVcLPSbZhDeuci3OSInVFPHirB3SN-AlrItkwbSKxXAe06otzth6UDRu21xyx2YJvq8P-bHwmiE5w=w447-h240-k-no"
+  },
+  {
+    title: "Paris",
+    location: "France",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "29 Aug, 2018",
+    endDate: "16 Sep, 2018",
+    description: "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine.",
+    imageUrl: "https://lh5.googleusercontent.com/p/AF1QipO1SBea_TAcD7HRdesG9CCMRk00Q8BZe_OIklEQ=w426-h240-k-no"
+  },
+]
